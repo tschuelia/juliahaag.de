81679ec020ac29fca9b019c005ff9466892ca0c6
Julia <julaschmid@googlemail.com>
2021-11-19 14:34:45 +0100

    wip:deplyoment

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, render_template
+import json
+
+app = Flask(__name__)
+
+
+clusters = {
+    "cascade": ("No data", ""),
+    "haswell": ("No data", ""),
+    "tom": ("No data", ""),
+    "jerry": ("No data", ""),
+    "phobos": ("No data", ""),
+    "deimos": ("No data", ""),
+    "amy": ("No data", ""),
+    "fry": ("No data", ""),
+}
+
+@app.route("/", methods=["GET"])
+def cv_homepage():
+    return render_template('index.html')
+
+@app.route("/monitoring", methods=["GET", "POST"])
+def monitor_clusters():
+    if request.method == "POST":
+        data = request.get_json()
+        cluster = data["cluster"]
+        info = data["info"]
+        time = data["time"]
+        clusters[cluster] = (info, time)
+        return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+
+    else:
+        return render_template('monitoring.html', clusters=clusters)
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from monitoring import app
+from app import app
 
 if __name__ == "__main__":
   app.run()
